1051cd22cdf0676742cde8e75c20f8e0d6ebeea9
Tebogo Lesedi <136654639+TebogoLesedi1@users.noreply.github.com>
2026-07-20 23:44:00 +0000

    Add files via upload

---
+++ 0.3stg_customer.sql
@@ -0,0 +1,34 @@
+CREATE TABLE dbo.DimCustomer
+(
+    CustomerKey INT IDENTITY(1,1) PRIMARY KEY,
+    [customer_first_name] VARCHAR(100),
+    [customer_last_name] VARCHAR(100),
+    [customer_email] VARCHAR(255),
+    [customer_phone] VARCHAR(30),
+    [customer_city] VARCHAR(100),
+    [customer_province] VARCHAR(100),
+    [customer_loyalty_tier] VARCHAR(50),
+    [Customer_Since] DATE
+);
+
+INSERT INTO dbo.DimCustomer
+(
+[customer_first_name],
+[customer_last_name],
+[customer_email],
+[customer_phone],
+[customer_city],
+[customer_province],
+[customer_loyalty_tier],
+[customer_since]
+)
+SELECT DISTINCT
+[customer_first_name],
+[customer_last_name],
+[customer_email],
+[customer_phone],
+[customer_city],
+[customer_province],
+[customer_loyalty_tier],
+[customer_since]
+FROM [stg].[dbo].[1782578019743_BrightLearn_Raw_Data];
