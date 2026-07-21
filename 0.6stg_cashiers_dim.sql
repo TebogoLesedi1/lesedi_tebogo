d16b39649446b9b40aea5f619823f7e92ffcd84f
Tebogo Lesedi <136654639+TebogoLesedi1@users.noreply.github.com>
2026-07-21 04:13:55 +0000

    Add files via upload

---
+++ 0.6stg_cashiers_dim.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS [stg].[dbo].[dim_cashier];
+
+CREATE TABLE dbo.dim_cashier
+(
+    CashierKey INT IDENTITY PRIMARY KEY,
+    Cashier_name VARCHAR(100)
+);
+
+INSERT INTO dbo.dim_cashier(Cashier_name)
+SELECT DISTINCT cashier_name
+FROM [stg].[dbo].[1782578019743_BrightLearn_Raw_Data];
